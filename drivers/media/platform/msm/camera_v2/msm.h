ea278985d417849cd27d81dbbfc062bed4191e1d
Trishansh Bhardwaj <tbhardwa@codeaurora.org>
2017-02-06 12:32:40 +0530

    msm: camera: Limit GPU clock on camera open
    On camera open, vote for kgsl power limit to decrease GPU clock.
    
    Change-Id: I8678409903b630ad6cdea5df472e08167789102c
    Signed-off-by: Trishansh Bhardwaj <tbhardwa@codeaurora.org>
    Signed-off-by: Lau <laststandrighthere@gmail.com>

--- drivers/media/platform/msm/camera_v2/msm.h
+++ drivers/media/platform/msm/camera_v2/msm.h
@@ -20,6 +20,7 @@
 #include <linux/pm_qos.h>
 #include <linux/msm_ion.h>
 #include <linux/iommu.h>
+#include <linux/msm_kgsl.h>
 #include <media/v4l2-dev.h>
 #include <media/v4l2-ioctl.h>
 #include <media/v4l2-device.h>
@@ -112,6 +113,7 @@ struct msm_session {
 	struct mutex lock_q;
 	struct mutex close_lock;
 	rwlock_t stream_rwlock;
+	struct kgsl_pwr_limit *sysfs_pwr_limit;
 };
 
 static inline bool msm_is_daemon_present(void)
