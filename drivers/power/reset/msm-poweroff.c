b8116b594bd0df5439fba6d080691dd364271c02
Artem Borisov <dedsa2002@gmail.com>
2018-02-07 18:46:50 +0300

    drivers: power: Import Xiaomi changes

--- drivers/power/reset/msm-poweroff.c
+++ drivers/power/reset/msm-poweroff.c
@@ -60,7 +60,7 @@ static void scm_disable_sdi(void);
 * There is no API from TZ to re-enable the registers.
 * So the SDI cannot be re-enabled when it already by-passed.
 */
-static int download_mode = 1;
+static int download_mode;
 #else
 static const int download_mode;
 #endif
@@ -204,12 +204,6 @@ static void set_dload_mode(int on)
 {
 	return;
 }
-
-static void enable_emergency_dload_mode(void)
-{
-	pr_err("dload mode is not enabled on target\n");
-}
-
 static bool get_dload_mode(void)
 {
 	return false;
@@ -300,7 +294,12 @@ static void msm_restart_prepare(const char *cmd)
 		qpnp_pon_system_pwr_off(PON_POWER_OFF_HARD_RESET);
 	}
 
-	if (cmd != NULL) {
+	if (in_panic) {
+		qpnp_pon_system_pwr_off(PON_POWER_OFF_WARM_RESET);
+		qpnp_pon_set_restart_reason(
+			PON_RESTART_REASON_PANIC);
+		__raw_writel(0x77665508, restart_reason);
+	} else if (cmd != NULL) {
 		if (!strncmp(cmd, "bootloader", 10)) {
 			qpnp_pon_set_restart_reason(
 				PON_RESTART_REASON_BOOTLOADER);
@@ -336,9 +335,19 @@ static void msm_restart_prepare(const char *cmd)
 		} else if (!strncmp(cmd, "edl", 3)) {
 			enable_emergency_dload_mode();
 #endif
+		} else if (!strcmp(cmd, "other")) {
+			qpnp_pon_set_restart_reason(
+				PON_RESTART_REASON_OTHER);
+			__raw_writel(0x77665501, restart_reason);
 		} else {
+				qpnp_pon_set_restart_reason(
+				PON_RESTART_REASON_NORMAL);
 			__raw_writel(0x77665501, restart_reason);
 		}
+	} else {
+		qpnp_pon_set_restart_reason(
+			PON_RESTART_REASON_NORMAL);
+		__raw_writel(0x77665501, restart_reason);
 	}
 
 	flush_cache_all();
@@ -404,6 +413,8 @@ static void do_msm_poweroff(void)
 	set_dload_mode(0);
 	scm_disable_sdi();
 	qpnp_pon_system_pwr_off(PON_POWER_OFF_SHUTDOWN);
+	qpnp_pon_set_restart_reason(PON_RESTART_REASON_UNKNOWN);
+	__raw_writel(0x0, restart_reason);
 
 	halt_spmi_pmic_arbiter();
 	deassert_ps_hold();
@@ -573,6 +584,11 @@ skip_sysfs_create:
 					   "tcsr-boot-misc-detect");
 	if (mem)
 		tcsr_boot_misc_detect = mem->start;
+
+	qpnp_pon_set_restart_reason(
+								PON_RESTART_REASON_UNKNOWN);
+	__raw_writel(0x77665510, restart_reason);
+
 
 	pm_power_off = do_msm_poweroff;
 	arm_pm_restart = do_msm_restart;
