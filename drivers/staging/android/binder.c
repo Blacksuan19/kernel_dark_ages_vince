bc8bf64ca011e5ee2343200cae899e439daa9cff
mdeejay <mdjrussia@gmail.com>
2018-05-15 17:26:33 +0700

    binder: fix random panics

--- drivers/staging/android/binder.c
+++ drivers/staging/android/binder.c
@@ -2109,13 +2109,16 @@ static void binder_cleanup_transaction(struct binder_transaction *t,
 				       const char *reason,
 				       uint32_t error_code)
 {
-	if (t->buffer->target_node && !(t->flags & TF_ONE_WAY)) {
+	if (t->buffer->target_node &&
+			    !(t->flags & TF_ONE_WAY)) {
 		binder_send_failed_reply(t, error_code);
 	} else {
 		binder_debug(BINDER_DEBUG_DEAD_TRANSACTION,
-			"undelivered transaction %d, %s\n",
-			t->debug_id, reason);
-		binder_free_transaction(t);
+			"binder: undelivered transaction %d\n",
+			t->debug_id);
+		t->buffer->transaction = NULL;
+		kfree(t);
+		binder_stats_deleted(BINDER_STAT_TRANSACTION);
 	}
 }
 
