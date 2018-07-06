df845952d9090c5204c9b80d8aaf1d25fa5a247e
Blacksuan19 <abubakaryagob@gmail.com>
2018-07-06 23:49:58 +0800

    GOV: remove duplicate
    Signed-off-by: Blacksuan19 <abubakaryagob@gmail.com>

--- drivers/cpufreq/cpufreq_governor.h
+++ drivers/cpufreq/cpufreq_governor.h
@@ -210,7 +210,6 @@ struct cs_dbs_tuners {
 	unsigned int down_threshold;
 	unsigned int freq_step;
 	 unsigned int down_threshold_suspended;
-	unsigned int freq_step;
 	unsigned int sleep_depth;
  	unsigned int boost_enabled;
  	unsigned int boost_count;
