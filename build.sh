f3c657bdcd77ba4aa7a517fc4dc3816fa382edaf
Blacksuan19 <abubakaryagob@gmail.com>
2018-07-20 21:22:02 +0800

    build: change name of treble builds
    Signed-off-by: Blacksuan19 <abubakaryagob@gmail.com>

--- build.sh
+++ build.sh
@@ -184,7 +184,12 @@ if [ "$choice" == "4" ]; then
   make clean &>/dev/null
   cp $KERN_IMG $ZIP_DIR/boot/zImage
   make &>/dev/null
+  if [[ type = "1" ]]; then
   make sign &>/dev/null
+  fi
+    if [[ type = "2" ]]; then
+  make sign-treble &>/dev/null
+  fi
   cd ..
   echo -e "$purple(i) Flashable zip generated under $ZIP_DIR.$nc"
   echo -e "$cyan#######################################################################$nc"
