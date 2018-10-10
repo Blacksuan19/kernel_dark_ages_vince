3e6327e0f058666a7182be1d6e9bf11c73884c98
Meera Gande <mgande@codeaurora.org>
2018-10-10 15:56:54 +0530

    msm: camera: isp: Reset request queue at stop
    In few scenarios where the buffers are not
    queued from HAL, request queue overflow is seen.
    Added changes to reset the queue at destroy and
    when the buffer is not available to process.
    
    Change-Id: I7239175dda9cbc26fb65f568cbc5f7183ceaa24d
    Signed-off-by: Meera Gande <mgande@codeaurora.org>

--- drivers/media/platform/msm/camera_v2/isp/msm_isp_axi_util.c
+++ drivers/media/platform/msm/camera_v2/isp/msm_isp_axi_util.c
@@ -13,6 +13,7 @@
 #include <media/v4l2-subdev.h>
 #include <asm/div64.h>
 #include "msm_isp_util.h"
+#include "msm_isp_stats_util.h"
 #include "msm_isp_axi_util.h"
 #include "trace/events/msm_cam.h"
 
@@ -116,6 +117,8 @@ void msm_isp_axi_destroy_stream(
 		axi_data->stream_info[stream_idx].stream_handle = 0;
 	} else {
 		pr_err("%s: stream does not exist\n", __func__);
+		memset(&axi_data->stream_info[stream_idx].request_queue_cmd,
+			0, sizeof(axi_data->stream_info[stream_idx].request_queue_cmd));
 	}
 }
 
@@ -3505,6 +3508,9 @@ static int msm_isp_request_frame(struct vfe_device *vfe_dev,
 			stream_info->undelivered_request_cnt--;
 			pr_err_ratelimited("%s:%d fail to cfg HAL buffer\n",
 				__func__, __LINE__);
+			queue_req->cmd_used = 0;
+			list_del(&queue_req->list);
+			stream_info->request_q_cnt--;
 			return rc;
 		}
 
@@ -3551,6 +3557,9 @@ static int msm_isp_request_frame(struct vfe_device *vfe_dev,
 						flags);
 			pr_err_ratelimited("%s:%d fail to cfg HAL buffer\n",
 				__func__, __LINE__);
+			queue_req->cmd_used = 0;
+			list_del(&queue_req->list);
+			stream_info->request_q_cnt--;
 			return rc;
 		}
 	} else {
