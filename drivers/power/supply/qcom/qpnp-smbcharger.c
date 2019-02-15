c3002f84d41c9ca43c9e4eed104a3d117ff0f096
Rama Bondan Prakoso <ramanarubp@gmail.com>
2019-02-15 05:33:59 +0000

    drivers: power: Squashed revert of tissot and mido changes
    Revert "power: qpnp-smbcharger: Increase the charge current"
    
    This reverts commit 56263e7992c0cdcfab4bcd4c8a934a01ef8782e2.
    
    Revert "power: qpnp-fg: Update battery soc value for tissot"
    
    This reverts commit 236a5afa65ab1c1a3e04b258fa9a2cbdc0779c05.
    
    Revert "drivers: power: Import tissot changes"
    
    This reverts commit 38c40eb5d35a902452f272d1bafd4f4e93905c69.
    
    Revert "drivers: power: Import mido changes"
    
    This reverts commit 201c5be1cad828bdde0f3a887c1b1ba2284da5b3.

--- drivers/power/supply/qcom/qpnp-smbcharger.c
+++ drivers/power/supply/qcom/qpnp-smbcharger.c
@@ -447,7 +447,7 @@ module_param_named(
 	int, 00600
 );
 
-static int smbchg_default_hvdcp_icl_ma = 2000;
+static int smbchg_default_hvdcp_icl_ma = 1800;
 module_param_named(
 	default_hvdcp_icl_ma, smbchg_default_hvdcp_icl_ma,
 	int, 00600
@@ -459,7 +459,7 @@ module_param_named(
 	int, 00600
 );
 
-static int smbchg_default_dcp_icl_ma = 2000;
+static int smbchg_default_dcp_icl_ma = 1800;
 module_param_named(
 	default_dcp_icl_ma, smbchg_default_dcp_icl_ma,
 	int, 00600
@@ -4764,9 +4764,6 @@ static bool is_usbin_uv_high(struct smbchg_chip *chip)
 	return reg &= USBIN_UV_BIT;
 }
 
-#ifdef CONFIG_MACH_XIAOMI_MIDO
-static int rerun_apsd(struct smbchg_chip *chip);
-#endif
 #define HVDCP_NOTIFY_MS		2500
 static void handle_usb_insertion(struct smbchg_chip *chip)
 {
@@ -4777,12 +4774,6 @@ static void handle_usb_insertion(struct smbchg_chip *chip)
 	pr_smb(PR_STATUS, "triggered\n");
 	/* usb inserted */
 	read_usb_type(chip, &usb_type_name, &usb_supply_type);
-#ifdef CONFIG_MACH_XIAOMI_MIDO
-	if (usb_supply_type == POWER_SUPPLY_TYPE_USB_CDP || usb_supply_type == POWER_SUPPLY_TYPE_USB) {
-		rc = rerun_apsd(chip);
-		read_usb_type(chip, &usb_type_name, &usb_supply_type);
-	}
-#endif
 	pr_smb(PR_STATUS,
 		"inserted type = %d (%s)", usb_supply_type, usb_type_name);
 
@@ -6048,11 +6039,7 @@ static int smbchg_battery_get_property(struct power_supply *psy,
 		val->intval = get_prop_batt_health(chip);
 		break;
 	case POWER_SUPPLY_PROP_TECHNOLOGY:
-#if (defined CONFIG_MACH_XIAOMI_MIDO) || (defined CONFIG_MACH_XIAOMI_TISSOT)
-		val->intval = POWER_SUPPLY_TECHNOLOGY_LIPO;
-#else
 		val->intval = POWER_SUPPLY_TECHNOLOGY_LION;
-#endif
 		break;
 	case POWER_SUPPLY_PROP_FLASH_CURRENT_MAX:
 		val->intval = smbchg_calc_max_flash_current(chip);
@@ -6259,17 +6246,6 @@ static irqreturn_t batt_warm_handler(int irq, void *_chip)
 {
 	struct smbchg_chip *chip = _chip;
 	u8 reg = 0;
-#if (defined CONFIG_MACH_XIAOMI_MIDO) || (defined CONFIG_MACH_XIAOMI_TISSOT)
-	int rc;
-	/* set the warm float voltage compensation,set the warm float voltage to 4.1V */
-	if (chip->float_voltage_comp != -EINVAL) {
-		rc = smbchg_float_voltage_comp_set(chip, chip->float_voltage_comp);
-	if (rc < 0)
-		dev_err(chip->dev, "Couldn't set float voltage comp rc = %d\n", rc);
-	pr_smb(PR_STATUS, "set float voltage comp to %d\n", chip->float_voltage_comp);
-
-	}
-#endif
 
 	smbchg_read(chip, &reg, chip->bat_if_base + RT_STS, 1);
 	chip->batt_warm = !!(reg & HOT_BAT_SOFT_BIT);
@@ -6286,14 +6262,6 @@ static irqreturn_t batt_cool_handler(int irq, void *_chip)
 {
 	struct smbchg_chip *chip = _chip;
 	u8 reg = 0;
-
-#if (defined CONFIG_MACH_XIAOMI_MIDO) || (defined CONFIG_MACH_XIAOMI_TISSOT)
-	int rc;
-	/* set the cool float voltage compensation ,set the cool float voltage to 4.4V*/
-	rc = smbchg_float_voltage_comp_set(chip, 0);
-	if (rc < 0)
-		dev_err(chip->dev, "Couldn't set float voltage comp rc = %d\n", rc);
-#endif
 
 	smbchg_read(chip, &reg, chip->bat_if_base + RT_STS, 1);
 	chip->batt_cool = !!(reg & COLD_BAT_SOFT_BIT);
@@ -7351,22 +7319,6 @@ static int smbchg_hw_init(struct smbchg_chip *chip)
 		if (rc < 0)
 			dev_err(chip->dev, "Couldn't set OTG OC config rc = %d\n",
 				rc);
-
-#ifdef CONFIG_MACH_XIAOMI_MIDO
-		rc = smbchg_sec_masked_write(chip, chip->otg_base + OTG_CFG, 0x0c, 0x8);
-		if (rc < 0) {
-			dev_err(chip->dev, "Couldn't set SMBCHGL_OTG_CFG rc=%d\n",
-				rc);
-		}
-
-		rc = smbchg_read(chip, &reg, chip->otg_base + OTG_CFG, 1);
-		printk("%s:read OTG_CFG=%2x\n", __func__, reg);
-		if (rc < 0) {
-			dev_err(chip->dev, "Couldn't set SMBCHGL_OTG_CFG rc=%d\n",
-				rc);
-		}
-#endif
-
 	}
 
 	if (chip->otg_pinctrl) {
@@ -8402,10 +8354,6 @@ static int smbchg_probe(struct platform_device *pdev)
 		dev_err(chip->dev, "Error parsing DT peripherals: %d\n", rc);
 		goto votables_cleanup;
 	}
-
-#if (defined CONFIG_MACH_XIAOMI_MIDO) || (defined CONFIG_MACH_XIAOMI_TISSOT)
-	chip->hvdcp_not_supported = true;
-#endif
 
 	rc = smbchg_check_chg_version(chip);
 	if (rc) {
