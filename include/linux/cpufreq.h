b90d3fd069dc28e346ee998fd5f64b56f7a399dd
Abubakar Yagoub <Abubakaryagob@gmail.com>
2019-03-09 15:59:05 +0800

    cpufreq: Add Electron governor
    Signed-off-by: Abubakar Yagoub <Abubakaryagob@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -516,6 +516,9 @@ extern struct cpufreq_governor cpufreq_gov_interactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SCHED)
 extern struct cpufreq_governor cpufreq_gov_sched;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_sched)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ELECTRON)
+extern struct cpufreq_governor cpufreq_gov_electron;
+#define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_electron)
 #endif
 
 /*********************************************************************
