ff8a9f3b44dcd6a72d2633812e49317f2f75b2a6
Abubakar Yagoub <Abubakaryagob@gmail.com>
2019-03-10 23:28:32 +0800

    cpu-boost: set min input boost interval to 100ms
    Signed-off-by: Abubakar Yagoub <Abubakaryagob@gmail.com>

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -36,7 +36,7 @@ static DEFINE_PER_CPU(struct cpu_sync, sync_info);
 static unsigned int input_boost_enabled = 1;
 module_param(input_boost_enabled, uint, 0644);
 
-static unsigned int input_boost_ms = 1500;
+static unsigned int input_boost_ms = 100;
 module_param(input_boost_ms, uint, 0644);
 
 static bool sched_boost_on_input;
