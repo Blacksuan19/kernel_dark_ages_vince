b77b64baa16fa74ba2c88a12d7e7c846bcf6ed11
Blacksuan19 <Abubakaryagob@gmail.com>
2019-06-20 23:32:38 +0800

    include: add missing state notifier vars
    Signed-off-by: Blacksuan19 <Abubakaryagob@gmail.com>

--- include/linux/state_notifier.h
+++ include/linux/state_notifier.h
@@ -3,16 +3,18 @@
 
 #include <linux/notifier.h>
 
-#define STATE_NOTIFIER_ACTIVE		0x01
-#define STATE_NOTIFIER_SUSPEND		0x02
+#define STATE_NOTIFIER_ACTIVE 0x01
+#define STATE_NOTIFIER_SUSPEND 0x02
+#define STATE_NOTIFIER_BOOST 0x03
 
 struct state_event {
-	void *data;
+  void *data;
 };
 
 extern bool state_suspended;
 extern void state_suspend(void);
 extern void state_resume(void);
+extern void state_boost(void);
 int state_register_client(struct notifier_block *nb);
 int state_unregister_client(struct notifier_block *nb);
 int state_notifier_call_chain(unsigned long val, void *v);
