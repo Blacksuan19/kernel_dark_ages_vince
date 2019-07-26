1f2fc27afc747e580d9d525063e93394bd3c4467
Benjamin Herrenschmidt <benh@kernel.crashing.org>
2019-07-26 14:59:03 +1000

    usb: gadget: composite: Clear "suspended" on reset/disconnect
    [ Upstream commit 602fda17c7356bb7ae98467d93549057481d11dd ]
    
    In some cases, one can get out of suspend with a reset or
    a disconnect followed by a reconnect. Previously we would
    leave a stale suspended flag set.
    
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>
    Signed-off-by: Felipe Balbi <felipe.balbi@linux.intel.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Blacksuan19 <Abubakaryagob@gmail.com>

--- drivers/usb/gadget/composite.c
+++ drivers/usb/gadget/composite.c
@@ -2195,12 +2195,8 @@ void composite_disconnect(struct usb_gadget *gadget)
 	 * disconnect callbacks?
 	 */
 	spin_lock_irqsave(&cdev->lock, flags);
-	if (cdev->config) {
-		if (gadget->is_chipidea && !cdev->suspended) {
-			spin_unlock_irqrestore(&cdev->lock, flags);
-			msm_do_bam_disable_enable(CI_CTRL);
-			spin_lock_irqsave(&cdev->lock, flags);
-		}
+	cdev->suspended = 0;
+	if (cdev->config)
 		reset_config(cdev);
 	}
 	if (cdev->driver->disconnect)
