e89403ba39bc004db93fc1cbe8a4b5e38446e007
utsavbalar1231 <utsavbalar1231@gmail.com>
2019-12-19 03:19:44 +0800

    arm64:vdso - Fix -Wstring-overflow warning
     warning: ‘memcmp’ reading 4 bytes from a region of size 1 [-Wstringop-overflow=]
      if (memcmp(&vdso_start, "\177ELF", 4))
    
    Signed-off-by: blacksuan19 <abubakaryagob@gmail.com>

--- arch/arm64/kernel/vdso.c
+++ arch/arm64/kernel/vdso.c
@@ -116,7 +116,7 @@ static int __init vdso_init(void)
 	int i;
 	unsigned long pfn;
 
-	if (memcmp(&vdso_start, "\177ELF", 4)) {
+	if (memcmp(&vdso_start, "\177ELF", 1)) {
 		pr_err("vDSO is not a valid ELF object!\n");
 		return -EINVAL;
 	}
