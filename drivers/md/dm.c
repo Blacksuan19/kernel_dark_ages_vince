131e56b671b845f81df8e1ea3e27ab3fa70914c5
blacksuan19 <i@blacksuan19.me>
2020-02-20 00:49:41 +0800

    drivers: md: properly point to pointer
    this is not java
    Signed-off-by: blacksuan19 <i@blacksuan19.me>

--- drivers/md/dm.c
+++ drivers/md/dm.c
@@ -1474,8 +1474,8 @@ void dm_init_normal_md_queue(struct mapped_device *md)
 	/*
 	 * Initialize aspects of queue that aren't relevant for blk-mq
 	 */
-	md->queue->backing_dev_info.congested_data = md;
-	md->queue->backing_dev_info.congested_fn = dm_any_congested;
+	md->queue->backing_dev_info->congested_data = md;
+	md->queue->backing_dev_info->congested_fn = dm_any_congested;
 	blk_queue_bounce_limit(md->queue, BLK_BOUNCE_ANY);
 }
 
