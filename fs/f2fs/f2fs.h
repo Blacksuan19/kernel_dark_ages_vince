8301468fd6f0802f19289fc6dfa3d171f611384c
blacksuan19 <i@blacksuan19.me>
2020-04-13 15:41:25 +0800

    f2fs: add missing f2fs_io_info members
    Signed-off-by: blacksuan19 <i@blacksuan19.me>

--- fs/f2fs/f2fs.h
+++ fs/f2fs/f2fs.h
@@ -1039,10 +1039,13 @@ struct f2fs_io_info {
 	bool submitted;		/* indicate IO submission */
 	int need_lock;		/* indicate we need to lock cp_rwsem */
 	bool in_list;		/* indicate fio is in io_list */
-	bool is_meta;		/* indicate borrow meta inode mapping or not */
+    bool is_meta;           /* indicate borrow meta inode mapping or not */
+	bool is_por;		/* indicate IO is from recovery or not */
 	bool retry;		/* need to reallocate block address */
 	enum iostat_type io_type;	/* io type */
 	struct writeback_control *io_wbc; /* writeback control */
+	struct bio **bio;		/* bio for ipu */
+	sector_t *last_block;		/* last block number in bio */
 	unsigned char version;		/* version of the node */
 };
 
