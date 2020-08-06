800b7b70013c15eaa77b164a308e269c6d8e3dd3
Lau <laststandrighthere@gmail.com>
2020-08-06 23:59:35 +0800

    sched: tune: Adjust do_prefer_idle() to new code style
    Signed-off-by: Lau <laststandrighthere@gmail.com>

--- kernel/sched/tune.c
+++ kernel/sched/tune.c
@@ -1347,7 +1347,7 @@ int do_stune_boost(char *st_name, int boost, int *slot)
 
 int do_prefer_idle(char *st_name, u64 prefer_idle)
 {
-	struct schedtune *st = getSchedtune(st_name);
+	struct schedtune *st = stune_get_by_name(st_name);
 
 	if (!st)
 		return -EINVAL;
